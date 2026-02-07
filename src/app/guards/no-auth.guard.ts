65d69c9ac303a155a78ac64b49e15571139abddc
Am0r123 <amrwael654@gmail.com>
2026-02-07 23:47:36 +0200

    guards

---
+++ src/app/guards/no-auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoAuthGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+
+  canActivate(): boolean {
+    const isLoggedIn = !!localStorage.getItem('token'); 
+
+    if (isLoggedIn) {
+      this.router.navigate(['/dashboard']);
+      return false;
+    }
+
+    return true;
+  }
+}
